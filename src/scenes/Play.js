88281a6f347b4985b747834934f465282d79d3ea
Ethan Chen <ethchen@ucsc.edu>
2021-05-16 14:15:58 -0700

    -(temporary?) object placement by pressing space -collision between player and object (unrestricted velocity) -temp tower and enemy on screen (tower will need to be bigger)

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,6 +10,9 @@ class Play extends Phaser.Scene
         //load sprites
         this.load.image('player','./assets/tempPlayer.png');
         this.load.image('background', './assets/tempBackground.png');
+        this.load.image('object','./assets/tempObject.png');
+        this.load.image('enemy','./assets/tempEnemy.png');
+        this.load.image('maintower','./assets/tempTower.png');
     }
 
     create()
@@ -23,8 +26,14 @@ class Play extends Phaser.Scene
         //placeholder title text
         this.add.text(20, 20, "Cat Tower Play");
 
+        //tower sprite placement
+        this.mainTower = this.physics.add.sprite(game.config.width/2,game.config.height / 2,'maintower').setOrigin(0,0);
+
+        //enemy sprite placement (temporary?)
+        this.enemy = this.physics.add.sprite(game.config.width / 4,game.config.height / 4,'enemy').setOrigin(0,0);
+
         //player sprite placement
-        this.player = this.physics.add.sprite(game.config.width/2,game.config.height/2,'player').setOrigin(0,0);
+        this.player = this.physics.add.sprite(game.config.width/2,game.config.height * 3/4,'player').setOrigin(0,0);
 
         //movement mechanic, move towards cursor position at time of left click
         this.input.on('pointerdown', function (pointer)
@@ -35,6 +44,9 @@ class Play extends Phaser.Scene
 
             this.physics.moveToObject(this.player,pointer,500);
         },this);
+
+        //define other keys
+        keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
     update()
@@ -47,6 +59,15 @@ class Play extends Phaser.Scene
             {
                 this.player.body.reset(this.target.x,this.target.y);
             }
+        }
+
+        //place object at player position when pressing space (if they are not moving)
+        if (Phaser.Input.Keyboard.JustDown(keySPACE) && this.distance < 4) {
+            this.object1 = this.physics.add.sprite(this.player.x + (this.player.width / 4), this.player.y - (this.player.height * 3/4), 'object').setOrigin(0,0);
+
+            //object collision
+            this.physics.add.collider(this.player, this.object1);
+            this.physics.add.collider(this.enemy, this.object1);
         }
     }
 }
