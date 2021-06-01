1cb8a8622a5e7f5b55c9d53f59908638ad86e698
Mathew Aceves <mraceves@ucsc.edu>
2021-06-01 13:06:55 -0700

    main menu art

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,6 +7,7 @@ class Menu extends Phaser.Scene
 
     preload()
     {
+        this.load.image('mainMenu','./assets/mainMenu.jpg');
         this.load.audio('squeak','./assets/squeak.wav');
         this.load.audio('place','./assets/place.wav');
         this.load.audio('bell','./assets/bell.wav');
@@ -15,13 +16,17 @@ class Menu extends Phaser.Scene
         this.load.audio('meow2','./assets/meow2.wav');
         this.load.audio('meow3','./assets/meow3.wav');
         this.load.audio('meow4','./assets/meow4.wav');
+        
     }
 
     create()
     {
+        
         //placeholder title text
         this.add.text(20, 20, "Cat Tower Main Menu");
         this.add.text(20, 60, "Press SPACE to begin");
+        //main menu art/title text
+        this.add.tileSprite(0,0,800,800,'mainMenu').setOrigin(0,0);
 
         //declare input variables
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
